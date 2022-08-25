7684e994ed982bf0e55462ed47049daeafbec87d
David Lee <davidtlee@gmail.com>
2022-08-25 00:35:14 -0700

    Setup inputs/outputs for long term goals

--- src/app/main/page/widget/widget.component.ts
+++ src/app/main/page/widget/widget.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { WidgetAnimations } from './widget.animations';
+import { LongTermGoal } from '../../../core/store/long-term-goal/long-term-goal.model';
 
 @Component({
   selector: 'app-widget',
@@ -10,6 +11,17 @@ import { WidgetAnimations } from './widget.animations';
 })
 export class WidgetComponent implements OnInit {
   // --------------- INPUTS AND OUTPUTS ------------------
+
+  /** The long term goal. */
+  @Input() longTermGoal: LongTermGoal = {
+    __id: 'ltg',
+    __userId: 'test-user',
+    oneYear: 'Improve my grades',
+    fiveYear: 'Land a well paying job that I enjoy',
+  };
+    
+  /** Initiate edit of long term goals. */
+  @Output() editGoals: EventEmitter<void> = new EventEmitter<void>();
 
   // --------------- LOCAL AND GLOBAL STATE --------------
 
