cadb9da12778d0b0cca76f86a99aa922a74b0c45
David Lee <davidtlee@gmail.com>
2022-08-24 23:11:18 -0700

    Setup inputs for datetime

--- src/app/main/page/widget/widget.component.ts
+++ src/app/main/page/widget/widget.component.ts
@@ -11,6 +11,9 @@ import { WidgetAnimations } from './widget.animations';
 export class WidgetComponent implements OnInit {
   // --------------- INPUTS AND OUTPUTS ------------------
 
+  /** Raw time in milliseconds from 1970/01/01 00:00:00:000 **/
+  @Input() time: number = 1661232134699;
+
   // --------------- LOCAL AND GLOBAL STATE --------------
 
   // --------------- DATA BINDING ------------------------
