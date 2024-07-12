97118313d5bb581b6f82fe050f489f42c675f972
audsostrom <audreyostrom64@gmail.com>
2024-07-11 22:30:25 -0700

    fix(general): rebased from other prior branch

--- src/app/general/home/weekly-goals/weekly-goals.component.ts
+++ src/app/general/home/weekly-goals/weekly-goals.component.ts
@@ -1,25 +1,10 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatDialogClose } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { WeeklyGoalItemComponent } from './weekly-goal-item/weekly-goal-item.component';
 import { WeeklyGoalsModalComponent } from './weekly-goals-modal/weekly-goals-modal.component';
 import { WeeklyGoalsAnimations } from './weekly-goals.animations';
-import { MatButton, MatIconButton } from '@angular/material/button';
-import { NgFor, NgOptimizedImage, NgStyle } from '@angular/common';
-import { WeeklyGoal } from 'src/app/core/store/weekly-goal/weekly-goal.model';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { CdkDropList, CdkDrag, CdkDragHandle } from '@angular/cdk/drag-drop';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatOption } from '@angular/material/core';
-import { MatDialogClose } from '@angular/material/dialog';
-import { MatFormField } from '@angular/material/form-field';
-import { MatIcon } from '@angular/material/icon';
-import { MatInput } from '@angular/material/input';
-import { MatSelect } from '@angular/material/select';
-import { MatTooltip } from '@angular/material/tooltip';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CoreModule } from 'src/app/core/core.module';
-import { MainModule } from 'src/app/main/main.module';
-import { SharedModule } from 'src/app/shared/shared.module';
 
 @Component({
   selector: 'app-weekly-goals',
