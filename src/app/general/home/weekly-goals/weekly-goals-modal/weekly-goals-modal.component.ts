e18aa76cb273e56d553e9c8f8fcb158c9e829b00
audsostrom <audreyostrom64@gmail.com>
2024-07-11 17:56:07 -0700

    feat(general): added events
    feat(general): updated events
    
    fix(general): adding in matdialogclose
    
    fix(general): adding in matdialogclose

--- src/app/general/home/weekly-goals/weekly-goals-modal/weekly-goals-modal.component.ts
+++ src/app/general/home/weekly-goals/weekly-goals-modal/weekly-goals-modal.component.ts
@@ -1,5 +1,17 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
 import { WeeklyGoalsModalAnimations } from './weekly-goals-modal.animations';
+import { MAT_DIALOG_DATA, MatDialogClose, MatDialogContent, MatDialogRef } from '@angular/material/dialog';
+import { QuarterlyGoalData, WeeklyGoalData } from '../../home.model';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatInput, MatInputModule } from '@angular/material/input';
+import { MatIcon } from '@angular/material/icon';
+import { endOfWeek, startOfWeek } from 'src/app/core/utils/time.utils';
+import { MatOption } from '@angular/material/core';
+import { NgFor } from '@angular/common';
+import { CdkDragHandle } from '@angular/cdk/drag-drop';
+import { MatSelect } from '@angular/material/select';
+import { MatTooltip } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-weekly-goals-modal',
@@ -9,6 +21,18 @@ import { WeeklyGoalsModalAnimations } from './weekly-goals-modal.animations';
   animations: WeeklyGoalsModalAnimations,
   standalone: true,
   imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogClose,
+    MatIcon,
+    MatDialogContent,
+    MatOption,
+    NgFor,
+    MatFormField,
+    MatInput,
+    MatSelect,
+    MatOption,
   ],
 })
 export class WeeklyGoalsModalComponent implements OnInit {
@@ -16,17 +40,32 @@ export class WeeklyGoalsModalComponent implements OnInit {
   // --------------- INPUTS AND OUTPUTS ------------------
 
   // --------------- LOCAL UI STATE ----------------------
+  
+  weeklyGoalsForm!: FormGroup;
 
   // --------------- COMPUTED DATA -----------------------
 
+  startOfWeek = startOfWeek;
+
+  endOfWeek = endOfWeek
+
   // --------------- EVENT HANDLING ----------------------
+
   
   // --------------- OTHER -------------------------------
 
   constructor(
+    @Inject(MAT_DIALOG_DATA) public data: {
+      goal: WeeklyGoalData,
+      allQuarterlyGoals: Partial<QuarterlyGoalData>[],
+    },
+    public dialogRef: MatDialogRef<WeeklyGoalsModalComponent>,
+    private fb: FormBuilder,
   ) { }
 
+
   // --------------- LOAD AND CLEANUP --------------------
+  
   ngOnInit(): void {
   }
 }
--- src/app/general/home/weekly-goals/weekly-goals.component.ts
+++ src/app/general/home/weekly-goals/weekly-goals.component.ts
@@ -2,10 +2,24 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { WeeklyGoalItemComponent } from './weekly-goal-item/weekly-goal-item.component';
 import { WeeklyGoalsModalComponent } from './weekly-goals-modal/weekly-goals-modal.component';
 import { WeeklyGoalsAnimations } from './weekly-goals.animations';
-import { MatButton } from '@angular/material/button';
-import { NgOptimizedImage, NgStyle } from '@angular/common';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { NgFor, NgOptimizedImage, NgStyle } from '@angular/common';
 import { WeeklyGoal } from 'src/app/core/store/weekly-goal/weekly-goal.model';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { CdkDropList, CdkDrag, CdkDragHandle } from '@angular/cdk/drag-drop';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatOption } from '@angular/material/core';
+import { MatDialogClose } from '@angular/material/dialog';
+import { MatFormField } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatInput } from '@angular/material/input';
+import { MatSelect } from '@angular/material/select';
+import { MatTooltip } from '@angular/material/tooltip';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from 'src/app/core/core.module';
+import { MainModule } from 'src/app/main/main.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @Component({
   selector: 'app-weekly-goals',
@@ -16,11 +30,11 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   standalone: true,
   imports: [
     MatButton,
-    NgStyle,
-    NgOptimizedImage,
     /** component */
     WeeklyGoalItemComponent,
     WeeklyGoalsModalComponent,
+    MatIconButton,
+    MatDialogClose,
   ],
 })
 export class WeeklyGoalsComponent implements OnInit {
@@ -32,19 +46,6 @@ export class WeeklyGoalsComponent implements OnInit {
   // --------------- COMPUTED DATA -----------------------
 
   // --------------- EVENT HANDLING ----------------------
-
-    /** Update weekly goal. */
-    async checkGoal(goal: WeeklyGoal) {
-      this.snackBar.open(
-        `Clicked on goal "${goal.text}"`,
-        '',
-        {
-          duration: 3000,
-          verticalPosition: 'bottom',
-          horizontalPosition: 'center',
-        },
-    );
-    }
   
   // --------------- OTHER -------------------------------
 
